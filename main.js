7825bc349cef7d859772356255dd9243e9b175c7
sara segura <sarasegura224@gmail.com>
2023-08-31 09:04:08 -0500

    Se añadio la funcionalidad de recargar las imagenes desde el borton de recarga de la sesión loadRandomMichis.

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
-console.log("Hello, world");
+const API_URL_RANDOM =
+  "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_xKPuRURI7sdbuuiXBuxx5xSgLJ7o4oYRBRVALsfStuudueWUUBbw6QHDUDWvjSEn";
 
-const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3";
+const API_URL_FAVORITES =
+  "https://api.thecatapi.com/v1/favourites?limit=3&api_key=live_xKPuRURI7sdbuuiXBuxx5xSgLJ7o4oYRBRVALsfStuudueWUUBbw6QHDUDWvjSEn";
 
-async function reload() {
-  const res = await fetch(API_URL);
+const spanError = document.getElementById("error");
+
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
-
+  console.log("Random");
   console.log(data);
-  const img1 = document.getElementById("img1");
-  const img2 = document.getElementById("img2");
-  const img3 = document.getElementById("img3");
 
-  img1.src = data[0].url;
-  img2.src = data[1].url;
-  img3.src = data[2].url;
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    const img3 = document.getElementById("img3");
+
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+  }
 }
 
-reload();
+async function loadFavouriteMichis() {
+  const res = await fetch(API_URL_FAVORITES);
+  const data = await res.json();
+  console.log("Favoritos");
+  console.log(data);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
+async function saveFavouriteMichis() {
+  const rest = await fetch(API_URL_FAVORITES, {
+    method: "POST",
+    header: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  console.log("save");
+  console.log(rest);
+}
+
+loadRandomMichis();
+loadFavouriteMichis();
